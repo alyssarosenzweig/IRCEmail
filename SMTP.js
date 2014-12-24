d7783fb547faf5167d2777011d286e58975c624e
bobbybee <j104@rocketmail.com>
2014-12-24 13:29:50 -0500

    misc changes

--- SMTP.js
+++ SMTP.js
@@ -3,7 +3,8 @@
 
 var net = require('net');
 
-var motd = "220 localhost SMTP IRCMail\r\n";
+var host = "localhost";
+var motd = "220 "+host+" ESMTP IRCMail\r\n";
 
 net.createServer(function(conn) {
 	conn.write(motd);
@@ -24,6 +25,14 @@ net.createServer(function(conn) {
 				dataMode = false;
 				conn.write("250 OK\r\n");
 
+				var letter = {
+					from: from,
+					to: to,
+					body: body
+				};
+
+				console.log(JSON.stringify(letter));
+
 				// prevent leaking for other messages
 				body = "";
 				from = "";
@@ -35,6 +44,8 @@ net.createServer(function(conn) {
 
 				if(commands[0] == "HELO") {
 					conn.write("250 Hello "+commands[1]+", I am glad to meet you.\r\n");
+				} else if(commands[0] == "EHLO") {
+					conn.write("250 "+host+" isn't really extended!\r\n");
 				} else if(commands[0] == "MAIL") {
 					conn.write("250 Ok.\r\n");
 					from = commands[1].slice(6, -1);
@@ -42,7 +53,7 @@ net.createServer(function(conn) {
 					conn.write("250 Ok.\r\n");
 					to.push(commands[1].slice(4, -1));
 				} else if(commands[0] == "DATA") {
-					conn.write("354 End data with \r\n.\r\n");
+					conn.write("354 End data with CRLF.CRLF\r\n");
 					dataMode = true;
 				} else if(commands[0] == "QUIT") {
 					conn.write("221 Bye\r\n");
@@ -50,4 +61,4 @@ net.createServer(function(conn) {
 			})
 		}
 	});
-})
+}).listen(25);
