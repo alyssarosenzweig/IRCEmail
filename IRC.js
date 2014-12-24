6f7b38477509a332eb55d23a6ff8af28e6de7bd9
bobbybee <j104@rocketmail.com>
2014-12-24 10:53:57 -0500

    Working IRC server stub

--- IRC.js
+++ IRC.js
@@ -3,8 +3,11 @@
 
 var net = require('net');
 
-function IRCServer() {
+function IRCServer(host) {
 	var that = this;
+
+	this.host = host;
+	console.log(host);
 
 	this.socket = net.createServer(function(connection) {
 		that.onConnection(connection);
@@ -17,6 +20,7 @@ IRCServer.prototype.listen = function(port) {
 
 IRCServer.prototype.onConnection = function(connection) {
 	console.log("New connection!");
+	connection.mask = ":mysterious";
 
 	var that = this;
 
@@ -30,7 +34,32 @@ IRCServer.prototype.onConnection = function(connection) {
 }
 
 IRCServer.prototype.onData = function(connection, data) {
-	console.log(data);
+	var that = this;
+
+	data.toString().split("\r\n").map(function(line) {
+		var commands = line.split(" ");
+		var cmd = commands[0];
+
+		console.log(commands);
+
+		if(cmd == "USER") {
+			connection.write(that.host+" 001 "+connection.nick+" :Welcome to IRCEmail. Join #"+commands[1]+" for your email!\r\n");
+			connection.write(that.host+" 002 "+connection.nick+" :Your host is "+that.host+" on IRCEmail\r\n");
+			connection.write(that.host+" 003 "+connection.nick+" :This server is as old as the epoch.. not really\r\n");
+			connection.write(that.host+" 004 "+connection.nick+" "+(that.host.slice(1))+" "+"IRCEmail0.1"+" "+"i o\r\n");
+
+			connection.write(that.host+" MODE "+connection.nick+" :+i\r\n");
+		} else if(cmd == "NICK") {
+			connection.nick = commands[1];
+			connection.mask = connection.nick+"!~"+connection.nick+"@unaffiliated/"+connection.nick;
+		} else if(cmd == "JOIN") {
+			connection.write(":"+connection.mask+" JOIN "+commands[1]+"\r\n");
+			connection.write(that.host+" 332 "+connection.nick+" "+commands[1]+" :Authenticate yourself\r\n");
+			connection.write(that.host+" 333 "+connection.nick+" "+commands[1]+" email 0\r\n");
+			connection.write(that.host+" 353 "+connection.nick+" = "+commands[1]+" :"+connection.nick+"\r\n");
+			connection.write(that.host+" 366 "+connection.nick+" "+commands[1]+" :End of /NAMES list."+"\r\n");
+		}
+	});
 }
 
 IRCServer.prototype.onEnd = function(connection) {
