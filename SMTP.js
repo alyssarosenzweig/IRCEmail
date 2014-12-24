69e2f02c522854f4baffc6214b6379a34bf7d071
bobbybee <j104@rocketmail.com>
2014-12-24 13:47:33 -0500

    Modularized it.. ish

--- SMTP.js
+++ SMTP.js
@@ -3,65 +3,65 @@
 
 var net = require('net');
 
-var host = "localhost";
-var motd = "220 "+host+" ESMTP IRCMail\r\n";
+function SMTPServer(host) {
+	var motd = "220 "+host+" ESMTP IRCMail\r\n";
+	this.inbox = {};
 
-var inbox = {};
+	net.createServer(function(conn) {
+		conn.write(motd);
 
-net.createServer(function(conn) {
-	conn.write(motd);
+		var dataMode = false;
+		var body = "";
+		var from = "";
+		var to = [];
 
-	var dataMode = false;
-	var body = "";
-	var from = "";
-	var to = [];
+		conn.on('data', function(data) {
+			console.log(data.toString());
 
-	conn.on('data', function(data) {
-		console.log(data.toString());
+			if(dataMode) {
+				var parts = data.toString().split("\r\n.\r\n");
+				body += parts[0];
 
-		if(dataMode) {
-			var parts = data.toString().split("\r\n.\r\n");
-			body += parts[0];
+				if(parts.length > 1) {
+					dataMode = false;
+					conn.write("250 OK\r\n");
 
-			if(parts.length > 1) {
-				dataMode = false;
-				conn.write("250 OK\r\n");
+					var letter = {
+						from: from,
+						to: to,
+						body: body
+					};
 
-				var letter = {
-					from: from,
-					to: to,
-					body: body
-				};
+					if(!this.inbox[to]) this.inbox[to] = [];
+					this.inbox[to].push(letter);
 
-				if(!inbox[to]) inbox[to] = [];
-				inbox[to].push(letter);
-
-				// prevent leaking for other messages
-				body = "";
-				from = "";
-				to = [];
-			}
-		} else {
-			data.toString().split("\r\n").map(function(line) {
-				var commands = line.split(' ');
-
-				if(commands[0] == "HELO") {
-					conn.write("250 Hello "+commands[1]+", I am glad to meet you.\r\n");
-				} else if(commands[0] == "EHLO") {
-					conn.write("250 "+host+" isn't really extended!\r\n");
-				} else if(commands[0] == "MAIL") {
-					conn.write("250 Ok.\r\n");
-					from = commands[1].slice(6, -1);
-				} else if(commands[0] == "RCPT") {
-					conn.write("250 Ok.\r\n");
-					to.push(commands[1].slice(4, -1));
-				} else if(commands[0] == "DATA") {
-					conn.write("354 End data with CRLF.CRLF\r\n");
-					dataMode = true;
-				} else if(commands[0] == "QUIT") {
-					conn.write("221 Bye\r\n");
+					// prevent leaking for other messages
+					body = "";
+					from = "";
+					to = [];
 				}
-			})
-		}
-	});
-}).listen(25);
+			} else {
+				data.toString().split("\r\n").map(function(line) {
+					var commands = line.split(' ');
+
+					if(commands[0] == "HELO") {
+						conn.write("250 Hello "+commands[1]+", I am glad to meet you.\r\n");
+					} else if(commands[0] == "EHLO") {
+						conn.write("250 "+host+" isn't really extended!\r\n");
+					} else if(commands[0] == "MAIL") {
+						conn.write("250 Ok.\r\n");
+						from = commands[1].slice(6, -1);
+					} else if(commands[0] == "RCPT") {
+						conn.write("250 Ok.\r\n");
+						to.push(commands[1].slice(4, -1));
+					} else if(commands[0] == "DATA") {
+						conn.write("354 End data with CRLF.CRLF\r\n");
+						dataMode = true;
+					} else if(commands[0] == "QUIT") {
+						conn.write("221 Bye\r\n");
+					}
+				})
+			}
+		});
+	}).listen(25);
+}
