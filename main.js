48c8a34bc3d031593cb16045b9d0b0b7f997b088
bobbybee <j104@rocketmail.com>
2014-12-24 14:06:51 -0500

    Bugfix: missing name

--- main.js
+++ main.js
@@ -12,17 +12,17 @@ var ircserver = new IRC(":"+host, "EMAIL", function(connection, channel, message
 		var account = smtpserver.inbox[channel];
 
 		if(!account) {
-			connection.write(":"+server.mask+" PRIVMSG "+channel+" :No such account\r\n");
+			connection.write(":"+ircserver.mask+" PRIVMSG "+channel+" :No such account\r\n");
 		} else {
-			connection.write(":"+server.mask+" PRIVMSG "+channel+" :You have "+account.length+" messages\r\n");
+			connection.write(":"+ircserver.mask+" PRIVMSG "+channel+" :You have "+account.length+" messages\r\n");
 		}
 
 	} else {
 		if(message == "test") {
-			connection.write(":"+server.mask+" PRIVMSG "+channel+" :Authenticating with password "+message+"\r\n");
+			connection.write(":"+ircserver.mask+" PRIVMSG "+channel+" :Authenticating with password "+message+"\r\n");
 			connection.authed.push(channel);
 		} else {
-			connection.write(":"+server.mask+" PRIVMSG "+channel+" :Uh oh! That's not right :/\r\n");
+			connection.write(":"+ircserver.mask+" PRIVMSG "+channel+" :Uh oh! That's not right :/\r\n");
 		}
 	}
 });
